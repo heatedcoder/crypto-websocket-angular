d1f755c2cb0c0ce62188651e2a439aa18acf9648
ng196p <ng196p@att.com>
2018-01-22 00:28:47 -0600

    added difference column

--- js/controllers/exchange.controller.js
+++ js/controllers/exchange.controller.js
@@ -11,17 +11,21 @@
          if(!$scope.binance['btc']) $scope.binance['btc'] = {};
          if(!$scope.binance['eth']) $scope.binance['eth'] = {};
 
+         if(!$scope.diff) $scope.diff = {};
+         if(!$scope.diff['btc']) $scope.diff['btc'] = {};
+         if(!$scope.diff['eth']) $scope.diff['eth'] = {};
+
         var wsBinanceBTCUSD = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@ticker');
         var wsBinanceETHUSD = new WebSocket('wss://stream.binance.com:9443/ws/ethusdt@ticker');
+        var wsBinanceETHBTC = new WebSocket('wss://stream.binance.com:9443/ws/ethbtc@ticker');
         var wsGDAX = new WebSocket('wss://ws-feed.gdax.com/');
         
         var GDAXRequest = {
             "type": "subscribe",
             "product_ids": [
                 "BTC-USD",
-                "BTC-EUR",
                 "ETH-USD",
-                "ETH-EUR"
+                "ETH-BTC"
             ],
             "channels": [
                 "ticker"
@@ -41,6 +45,9 @@
                 else if(JSON.parse(ev.data).s == 'ETHUSDT') {
                     $scope.binance['eth']['usd'] = JSON.parse(ev.data).c ;
                 }
+                else if(JSON.parse(ev.data).s == 'ETHBTC') {
+                    $scope.binance['eth']['btc'] = JSON.parse(ev.data).c ;
+                }
                 $scope.$apply();
             }
         }
@@ -55,21 +62,60 @@
                 if(ev && ev.data && JSON.parse(ev.data).product_id == "BTC-USD") {
                     $scope.gdax['btc']['usd'] = JSON.parse(ev.data).price;
                 }
-                else if(ev && ev.data && JSON.parse(ev.data).product_id == "BTC-EUR") {
-                    $scope.gdax['btc']['eur'] = JSON.parse(ev.data).price;
-                }
                 else if(ev && ev.data && JSON.parse(ev.data).product_id == "ETH-USD") {
                     $scope.gdax['eth']['usd'] = JSON.parse(ev.data).price;
                 }
-                else if(ev && ev.data && JSON.parse(ev.data).product_id == "ETH-EUR") {
-                    $scope.gdax['eth']['eur'] = JSON.parse(ev.data).price;
+                else if(ev && ev.data && JSON.parse(ev.data).product_id == "ETH-BTC") {
+                    $scope.gdax['eth']['btc'] = JSON.parse(ev.data).price;
                 }
                 $scope.$apply();   
             }
         }
         $scope.getBinanceExchangeRate(wsBinanceBTCUSD);
-        $scope.getBinanceExchangeRate(wsBinanceETHUSD);
+        $scope.getBinanceExchangeRate(wsBinanceETHUSD); 
+        $scope.getBinanceExchangeRate(wsBinanceETHBTC);
         $scope.getGDAXExchangeRate(wsGDAX, GDAXRequest);
 
+        // btc-usd
+        var btcusdCallback = function(n, o) {
+            var a = parseFloat($scope.gdax.btc.usd);
+            var b = parseFloat($scope.binance.btc.usd);
+            if(a > b) {
+                $scope.diff.btc.usd = ((a - b)/a)*100;
+            }
+            else {
+                $scope.diff.btc.usd = ((b - a)/b)*100;
+            }
+        }
+        $scope.$watch('gdax.btc.usd', btcusdCallback);
+        $scope.$watch('binance.btc.usd', btcusdCallback);
+
+        //eth-usd
+        var ethusdCallback = function(n, o) {
+            var a = parseFloat($scope.gdax.eth.usd);
+            var b = parseFloat($scope.binance.eth.usd);
+            if(a > b) {
+                $scope.diff.eth.usd = ((a - b)/a)*100;
+            }
+            else {
+                $scope.diff.eth.usd = ((b - a)/b)*100;
+            }
+        }
+        $scope.$watch('gdax.eth.usd', ethusdCallback);
+        $scope.$watch('binance.eth.usd', ethusdCallback);
+
+        //eth-btc
+        var ethbtcCallback = function(n, o) {
+            var a = parseFloat($scope.gdax.eth.btc);
+            var b = parseFloat($scope.binance.eth.btc);
+            if(a > b) {
+                $scope.diff.eth.btc = ((a - b)/a)*100;
+            }
+            else {
+                $scope.diff.eth.btc = ((b - a)/b)*100;
+            }
+        }
+        $scope.$watch('gdax.eth.btc', ethbtcCallback);
+        $scope.$watch('binance.eth.btc', ethbtcCallback);
     }]);
 })()
